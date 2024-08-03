95d056954ee7a5610cca6b1e1e0d5a8680b8be77
raccoon254 <tomsteve187@gmail.com>
2024-08-03 20:41:27 +0300

    Return dicebear api image identicon

--- app/auth/register/route.js
+++ app/auth/register/route.js
@@ -8,12 +8,17 @@ export async function POST(req) {
     const { name, email, password } = await req.json()
     const hashedPassword = await bcrypt.hash(password, 10)
 
+    function createImage(name) {
+        return `https://api.dicebear.com/9.x/identicon/svg?seed=${name}`
+    }
+
     try {
         const user = await prisma.user.create({
             data: {
                 name,
                 email,
                 password: hashedPassword,
+                image: createImage(name)
             },
         })
         return NextResponse.json({ message: 'User created successfully' }, { status: 201 })
