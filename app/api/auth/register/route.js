fe6f0908d24a4b4b0a3120c4f3bcf581c789d3d9
raccoon254 <tomsteve187@gmail.com>
2024-08-03 21:40:53 +0300

    Added bycrpt for match passwords

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 import { NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
