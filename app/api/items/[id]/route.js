8580281654df487ba5cd0f61dcaf093ce9f5edfe
raccoon254 <tomsteve187@gmail.com>
2024-08-03 20:44:50 +0300

    Add parameter id endpoint for specific endpoint

---
+++ app/api/items/[id]/route.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+
+const prisma = new PrismaClient()
+
+export async function GET(req, { params }) {
+    const { id } = params
+    try {
+        const item = await prisma.item.findUnique({
+            where: { id },
+            include: {
+                seller: {
+                    select: {
+                        name: true,
+                        email: true,
+                    },
+                },
+            },
+        })
+        if (!item) {
+            return NextResponse.json({ message: 'Item not found' }, { status: 404 })
+        }
+        return NextResponse.json(item)
+    } catch (error) {
+        return NextResponse.json({ message: 'Error fetching item' }, { status: 500 })
+    }
+}
